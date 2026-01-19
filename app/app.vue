4921f31dcf280a6f343db4e3835b80bf8a57cca0
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2026-01-19 11:12:07 +0700

    add navbar

---
+++ app/app.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const isMounted = ref(false)
+  
+  onMounted(() => {
+    isMounted.value = true
+  })
+  </script>
+  
+  <template>
+    <div v-if="isMounted">
+      <Navbar />
+    </div>
+    
+    <div v-else class="h-screen w-screen flex items-center justify-center gap-2 bg-slate-950">
+      <UIcon name="i-heroicons-arrow-path" class="w-10 h-10 animate-spin text-primary-500" />
+    
+      <p class="text-slate-400 font-medium animate-pulse tracking-widest uppercase text-xs">
+        Loading Data
+      </p>
+    </div>
+  </template>
---
+++ app/components/Navbar.vue
@@ -0,0 +1,44 @@
+<script setup>
+    const colorMode = useColorMode()
+    const toggleTheme = () => {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+    const icon =computed(()=>{
+        return colorMode.value === 'dark' ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid';
+    })
+</script>
+
+<template>
+    <nav class="sticky top-0 z-50 w-full border-b border-slate-200 dark:border-slate-800 bg-white/80 dark:bg-slate-950 backdrop-blur-md">
+      <UContainer class="flex h-16 items-center justify-between">
+        <div class="flex items-center gap-2">
+          <div class="h-8 w-8 rounded-lg bg-primary-500 flex items-center justify-center text-white font-bold">
+            P
+          </div>
+          <span class="text-xl font-bold tracking-tight text-slate-900 dark:text-white">
+            Portfolio
+          </span>
+        </div>
+  
+        <div class="hidden md:flex items-center gap-10 text-sm font-medium text-slate-600 dark:text-slate-400">
+          <NuxtLink to="#about" class="hover:text-primary-500 transition-colors">About</NuxtLink>
+          <NuxtLink to="#skills" class="hover:text-primary-500 transition-colors">Skills</NuxtLink>
+          <NuxtLink to="#projects" class="hover:text-primary-500 transition-colors">Projects</NuxtLink>
+          <NuxtLink to="#contact" class="hover:text-primary-500 transition-colors">Contact</NuxtLink>
+        </div>
+  
+        <div class="flex items-center gap-4">
+            <UButton
+                :key = "colorMode.value"
+                :icon="icon"
+                color="gray"
+                variant="ghost"
+                @click="toggleTheme"
+                class="cursor-pointer"
+            />
+          
+          <UButton icon="i-heroicons-bars-3" color="gray" variant="ghost" class="md:hidden" />
+        </div>
+      </UContainer>
+    </nav>
+  </template>
